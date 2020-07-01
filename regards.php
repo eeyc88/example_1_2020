6994fdf6043c173f3f3ea64447e4cdb1d1c5bb6d
eeyc88 <erick-yepez-cedeno@hotmail.com>
2020-07-01 01:11:59 -0400

    update regards.php

--- regards.php
+++ regards.php
@@ -3,11 +3,21 @@
 
 <!DOCTYPE HTML>
 <html lang="en">
-<head>
-	<title>
-		DEMO
-	</title>
-</head>
-<body>
-</body>
+	<head>
+		<title>
+			DEMO
+		</title>
+	</head>
+	<body>
+		<header>
+			<meta></meta>
+		</header>
+
+
+		<footer>
+			<h6>
+				All rights reserved by Erick.
+			</h6>
+		</footer>
+	</body>
 </html>
