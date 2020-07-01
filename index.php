74b485204d2f425a3ec709e578ca278c4dea0c2e
eeyc88 <erick-yepez-cedeno@hotmail.com>
2020-07-01 01:57:17 -0400

    index.php fixed

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-	phpinfo();
 ?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<title>DEMO</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<header>
+			
+		</header>
+
+
+		<footer>
+			<h6>
+				All rights reserved by Erick.
+			</h6>
+		</footer>
+	</body>
+</html>
