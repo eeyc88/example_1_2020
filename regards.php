5a2d3c60cf88aafc65b5b5398a93c6c993af1f69
eeyc88 <erick-yepez-cedeno@hotmail.com>
2020-07-01 01:29:45 -0400

    fixing regards.php

--- regards.php
+++ regards.php
@@ -4,13 +4,12 @@
 <!DOCTYPE HTML>
 <html lang="en">
 	<head>
-		<title>
-			DEMO
-		</title>
+		<title>DEMO</title>
+		<meta charset="utf-8">
 	</head>
 	<body>
 		<header>
-			<meta></meta>
+			
 		</header>
 
 
